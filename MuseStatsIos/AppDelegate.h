126c50a3af3a96634fe10b499148ececbfbb1878
Anirudh Natarajan <anirudhnatarajan@Anirudhs-MacBook-Pro.local>
2017-02-05 04:11:34 -0800

    final

---
+++ MuseStatsIos/AppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  Kodikos, Inc. 2017
+//  MuseStatsIos
+//
+
+#import <UIKit/UIKit.h>
+#import <Muse/Muse.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
